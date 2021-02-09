b3152dc3fc18f21f058bb716fb811194ffbfd606
Arnav Bansal <arnavbansal.8232@gmail.com>
2021-02-09 15:03:28 +0530

    did not solve

--- Programming Using C++ (ECSE379L) - 3/Assignment/Lab 3/2.cpp
+++ Programming Using C++ (ECSE379L) - 3/Assignment/Lab 3/2.cpp
@@ -1,39 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <array>
 #include <utility>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
 int main() {
-    // int n=5;
-    // cin >> n;
-    // vector< pair<int, int>, n> v;
-    // int a, b;
-    // for(int i=0; i<n; i++){
-    //     cin >> a >> b;
-    //    vector_.push_back( make_pair(a,b) ); 
-    // }
+    int n=5;
+    cin >> n;
+    vector< pair<int, int>, n> v;
+    int a, b;
+    
+    for(int i=0; i<n; i++){
+        cin >> a >> b;
+       vector_.push_back( make_pair(a,b) ); 
+    }
 
-    // array< int, n > a{1,2,3,4,5};
    
     // for(int i=0; i<n; i++){
     //     cout << a[i] << " ";
     //     // cout << a[i].first << " " << a[i].second << endl;
     // }
 
-    int n=5;
-    array<int,n> myarray{ 0 }; 
-    cout << myarray.size(); 
-
-    for(int i=0; i<n; i++){
-        cout << myarray[i];
-    }
-    return 0; 
-
-
-
-//     int flag = 0;
+    //     int flag = 0;
 
 //     for (int i=0; i<n; i++) 
 //     { 

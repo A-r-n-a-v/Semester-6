c4119553dabcd83b733f127de194cbca0f44ac2d
Arnav Bansal <arnavbansal.8232@gmail.com>
2021-02-09 15:57:05 +0530

    lab 3 q3 (wrong output)

---
+++ Programming Using C++ (ECSE379L) - 3/Assignment/Lab 3/3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+
+    cout << "Enter number of records: ";
+    cin >> 3;
+
+    cout << "Total number of possible clustering arrangements is " <<  << endl;
+    
+
+    return 0;
+}
